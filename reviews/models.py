4ddba3f2b6bdd280c9e2f775bdf81cb3c903d9b5
Austin White <austingym@gmail.com>
2013-03-19 23:52:32 -0700

    Added and sync'd business model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Business(models.Model):
+    name            = models.CharField(max_length=100)
+    phone           = models.IntegerField()
+    website         = models.CharField(max_length=255)
+    address_street1 = models.CharField(max_length=100)
+    address_street2 = models.CharField(max_length=100)
+    address_city    = models.CharField(max_length=100)
+    address_state   = models.CharField(max_length=2)
+    address_zip     = models.IntegerField()
+
