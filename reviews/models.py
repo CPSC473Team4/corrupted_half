567cb585562308aadaa2da80624b2d6e96bd6d2f
Austin White <austingym@gmail.com>
2013-03-24 00:47:21 -0700

    Setting up models and relationships

--- reviews/models.py
+++ reviews/models.py
@@ -1,14 +1,31 @@
-from django.contrib.gis.db import models
+# Both packages import "models" so we need to alias them
+from django.contrib.gis.db import models as geoModels
+from django.db import models as djangoModels
 
-class Business(models.Model):
-    name            = models.CharField(max_length=100)
-    phone           = models.IntegerField()
-    website         = models.CharField(max_length=255)
-    address_street1 = models.CharField(max_length=100)
-    address_street2 = models.CharField(max_length=100)
-    address_city    = models.CharField(max_length=100)
-    address_state   = models.CharField(max_length=2)
-    address_zip     = models.IntegerField()
-    lon             = models.FloatField()
-    lat             = models.FloatField()
+# Import user model for relationships
+from django.contrib.auth.models import User
 
+class Business(geoModels.Model):
+	user            = geoModels.ForeignKey(User)
+	name            = geoModels.CharField(max_length=100)
+	phone           = geoModels.IntegerField()
+	website         = geoModels.CharField(max_length=255)
+	address_street1 = geoModels.CharField(max_length=100)
+	address_street2 = geoModels.CharField(max_length=100)
+	address_city    = geoModels.CharField(max_length=100)
+	address_state   = geoModels.CharField(max_length=2)
+	address_zip     = geoModels.IntegerField()
+	lon             = geoModels.FloatField()
+	lat             = geoModels.FloatField()
+
+
+class Category(djangoModels.Model):
+	name = djangoModels.CharField(max_length=100)
+
+class Business_Category(djangoModels.Model):
+	business = djangoModels.ForeignKey(Business)
+	category = djangoModels.ForeignKey(Category)
+
+class Review(djangoModels.Model):
+	business = djangoModels.ForeignKey(Business)
+	user = djangoModels.ForeignKey(User)
