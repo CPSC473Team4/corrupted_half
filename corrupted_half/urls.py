21cd7e5e02422a5e69c1c94da54826120d42b604
Austin White <austingym@gmail.com>
2013-03-27 01:49:39 -0700

    Added login functionality (will probably change urls to match django's LOGIN_URL and add more conformity) Modified home page to feature a big search and custom font

--- corrupted_half/urls.py
+++ corrupted_half/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from reviews.views import AuthLoginView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -9,6 +10,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'corrupted_half.views.home', name='home'),
     url(r'^$', 'reviews.views.home'),
 	url(r'^home/', 'reviews.views.home'),
+	url(r'^auth/(?P<action>\w+)', 'reviews.views.auth'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,44 @@
 # Create your views here.
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
+from django.views.generic.base import TemplateView
+
+from django.contrib.auth import authenticate, login
 
 def home(request):
     context = {}
     return render(request, 'reviews/index.html', context)
+
+def auth(request, action):
+	if request.method == 'POST':
+		if action == 'login':
+			username = request.POST['username']
+			password = request.POST['password']
+
+			user = authenticate(username=username, password=password)
+
+			if user is not None:
+				print(user.first_name)
+
+				if user.is_active:
+					login(request, user)
+					return redirect('/')
+				else:
+					# Disabled account
+					pass
+			else:
+				# Return an invalid login error message
+				pass
+	else:
+		if action == 'login':
+			return render(request, 'reviews/auth/login.html', {})
+		else:
+			return render(request, 'reviews/auth/logout.html', {})
+
+
+class AuthLoginView(TemplateView):
+	template_name = 'reviews/index.html'
+
+	def get(self, request, *args, **kwargs):
+		self.render_to_response({})
