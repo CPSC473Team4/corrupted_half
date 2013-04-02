1819fb3fdb77bd3f397d58888990eae66cbfb07e
Austin White <austingym@gmail.com>
2013-04-02 02:02:29 -0700

    Added new template for a business list. Business list uses pagination. Going to try to use this effect for business cards: http://tympanus.net/codrops/2012/06/18/3d-thumbnail-hover-effects/

--- corrupted_half/urls.py
+++ corrupted_half/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
-from reviews.views import BusinessView
+from reviews.views import BusinessListView
+
+from reviews.models import *
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -15,7 +17,7 @@ urlpatterns = patterns('',
 	url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 	url(r'^accounts/user/$', 'reviews.views.user', {}, name='view_user'),
 
-	url(r'^businesses/', BusinessView.as_view())
+	url(r'^businesses/$', BusinessListView.as_view()),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- reviews/views.py
+++ reviews/views.py
@@ -3,7 +3,13 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from reviews.models import Business
+
+from django.utils import timezone
 # from django.views.generic.base import View
 
 from django.contrib.auth import authenticate, login
@@ -16,11 +22,27 @@ def user(request):
     context = {}
     return render(request, 'reviews/user/view.html', context)
 
-class BusinessView(TemplateView):
-	template_name = "reviews/business/view.html"
+class BusinessListView(ListView):
+	model = Business
 
-	def get(self, request, *args, **kwargs):
-		return HttpResponse('Hello, World!')
+	def get_context_data(self, **kwargs):
+		business_list = Business.objects.all()
+		paginator = Paginator(business_list, 25)
+
+		page = self.request.GET.get('page')
+		try:
+			businesses = paginator.page(page)
+		except PageNotAnInteger:
+			# If page is not an integer, deliver first page.
+			businesses = paginator.page(1)
+		except EmptyPage:
+			# If page is out of range (e.g. 9999), deliver last page of results.
+			businesses = paginator.page(paginator.num_pages)
+
+		context = super(BusinessListView, self).get_context_data(**kwargs)
+
+		context['businesses'] = businesses
+		return context
 
 def auth(request, action):
 	if request.method == 'POST':
