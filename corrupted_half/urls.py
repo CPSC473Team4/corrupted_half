ea90855658f78d463298dc2f8e800a7b2f0ff915
CPSC 473 <cpsc473@cpsc473.(none)>
2013-04-08 11:39:00 -0700

    Added category implementation

--- corrupted_half/urls.py
+++ corrupted_half/urls.py
@@ -18,6 +18,7 @@ urlpatterns = patterns('',
 	url(r'^accounts/user/$', 'reviews.views.user', {}, name='view_user'),
 
 	url(r'^businesses/$', BusinessListView.as_view()),
+    url(r'^$', 'reviews.views.category'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- reviews/models.py
+++ reviews/models.py
@@ -1,29 +1,36 @@
 from django.db import models
 
 # Import user model for relationships
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
 class Business(models.Model):
-	user            = models.ForeignKey(User)
-	name            = models.CharField(max_length=100)
-	phone           = models.IntegerField()
-	website         = models.CharField(max_length=255)
-	address_street1 = models.CharField(max_length=100)
-	address_street2 = models.CharField(max_length=100)
-	address_city    = models.CharField(max_length=100)
-	address_state   = models.CharField(max_length=2)
-	address_zip     = models.IntegerField()
+   # user            = models.ForeignKey(User)
+    name            = models.CharField(max_length=100)
+    phone           = models.IntegerField()
+    website         = models.CharField(max_length=255)
+    address_street1 = models.CharField(max_length=100)
+    address_street2 = models.CharField(max_length=100)
+    address_city    = models.CharField(max_length=100)
+    address_state   = models.CharField(max_length=2)
+    address_zip     = models.IntegerField()
+    
+    def __unicode__(self):
+        return self.name
 
 class Review(models.Model):
-	business        = models.ForeignKey('Business')
-	rating          = models.IntegerField()
-	subject         = models.CharField(max_length=100)
-	body            = models.TextField()
+    business        = models.ForeignKey('Business')
+    rating          = models.IntegerField()
+    subject         = models.CharField(max_length=100)
+    body            = models.TextField()
 
+    def __unicode__(self):
+        return self.subject
+        
 class Category(models.Model):
-	name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    business = models.ManyToManyField(Business)
+    
+    def __unicode__(self):
+        return self.name
 
-class Business_Category(models.Model):
-	business = models.ForeignKey('Business')
-	category = models.ForeignKey('Category')
 
