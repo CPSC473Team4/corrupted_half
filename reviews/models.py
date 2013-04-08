09946d02bcb613a95db480a3e098e4140619f6dd
Austin White <austingym@gmail.com>
2013-04-08 11:51:26 -0700

    Converted tabs to spaces

--- reviews/models.py
+++ reviews/models.py
@@ -6,20 +6,20 @@ from django.core.urlresolvers import reverse
 
 class Business(models.Model):
     user            = models.ForeignKey(User)
-	name            = models.CharField(max_length=100)
-	phone           = models.IntegerField()
-	website         = models.CharField(max_length=255, null=True, blank=True)
-	address_street1 = models.CharField(max_length=100)
-	address_street2 = models.CharField(max_length=100, null=True, blank=True)
-	address_city    = models.CharField(max_length=100)
-	address_state   = models.CharField(max_length=2)
-	address_zip     = models.IntegerField()
-    
+    name            = models.CharField(max_length=100)
+    phone           = models.IntegerField()
+    website         = models.CharField(max_length=255, null=True, blank=True)
+    address_street1 = models.CharField(max_length=100)
+    address_street2 = models.CharField(max_length=100, null=True, blank=True)
+    address_city    = models.CharField(max_length=100)
+    address_state   = models.CharField(max_length=2)
+    address_zip     = models.IntegerField()
+
     def __unicode__(self):
         return self.name
 
-	def get_absolute_url(self):
-		return reverse('business_detail', kwargs={'pk': self.pk})
+    def get_absolute_url(self):
+        return reverse('business_detail', kwargs={'pk': self.pk})
 
 class Review(models.Model):
     business        = models.ForeignKey('Business')
@@ -29,11 +29,11 @@ class Review(models.Model):
 
     def __unicode__(self):
         return self.subject
-        
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
     business = models.ManyToManyField(Business)
-    
+
     def __unicode__(self):
         return self.name
 
