f6b6c1f9e4fa285cca90beba570874e15b3d0dd9
Austin White <austingym@gmail.com>
2013-04-12 02:24:56 -0700

    Trying different storage location

--- corrupted_half/settings.py
+++ corrupted_half/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'corrupted_half.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/usr/local/corrupted_half.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
